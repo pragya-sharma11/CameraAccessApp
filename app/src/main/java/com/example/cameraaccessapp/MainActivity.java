69f8398741f47f68baf5ca9c446553c5b0bac33c
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-04 22:33:36 +0530

    make functionality for clicking of button.

--- app/src/main/java/com/example/cameraaccessapp/MainActivity.java
+++ app/src/main/java/com/example/cameraaccessapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cameraaccessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         b1 = findViewById(R.id.button);
         i = findViewById(R.id.imageView);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+            }
+        });
 
     }
 }
