df77165929b1163ebf6e07ab4c6f095ea5d22f1e
github <pragya.sharma2_cs19@gla.ac.in>
2021-10-07 20:55:27 +0530

    make functionality for video player.

--- app/src/main/java/com/example/cameraaccessapp/MainActivity2.java
+++ app/src/main/java/com/example/cameraaccessapp/MainActivity2.java
@@ -2,13 +2,47 @@ package com.example.cameraaccessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.net.URI;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    Button b;
+    MediaController m;
+    VideoView v;
+    Uri u;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        b = findViewById(R.id.button3);
+        v = findViewById(R.id.videoView);
+        m = new MediaController(this);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(i, 0);
+            }
+        });
     }
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data){
+            super.onActivityResult(requestCode,resultCode,data);
+            u =data.getData();
+            v.setVideoURI(u);
+            v.setMediaController(m);
+            m.setAnchorView(v);
+            v.start();
+        }
+
 }
